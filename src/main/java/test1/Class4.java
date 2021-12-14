ddf97b4d52606df6845f0c7a0ac7204401b8ccf7
LianaYapparova <R29YMF0P2PS8IFQ>
2021-12-14 22:56:07 +0500

    second commit

---
+++ src/main/java/test1/Class4.java
@@ -0,0 +1,10 @@
+package test1;
+
+public class Class4 {
+    private static String name;
+    private static String name1;
+    private static String name2;
+    public static void main(String[] args) {
+
+    }
+}
