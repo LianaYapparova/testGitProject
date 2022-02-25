e4f7d0b162ac125b5b6f99405da50e04c8a23419
LianaYapparova <R29YMF0P2PS8IFQ>
2022-02-25 17:48:47 +0500

    return Test1

--- src/main/java/test1/Test1.java
+++ src/main/java/test1/Test1.java
@@ -6,9 +6,5 @@ public class Test1 {
         System.out.println(123);
         System.out.println(123);
         System.out.println(123);
-        for (int i = 0; i < 10; i++) {
-            System.out.println(i);
-
-        }
     }
 }
