71cc6b7a4c3c8e0f7727094988bcd5044bb19eed
LianaYapparova <R29YMF0P2PS8IFQ>
2022-02-25 17:03:44 +0500

    change Test1

--- src/main/java/test1/Test1.java
+++ src/main/java/test1/Test1.java
@@ -3,5 +3,8 @@ package test1;
 public class Test1 {
     public static void main(String[] args) {
         System.out.println(123);
+        System.out.println(123);
+        System.out.println(123);
+        System.out.println(123);
     }
 }
